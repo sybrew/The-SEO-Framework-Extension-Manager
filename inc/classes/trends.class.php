785f2eddfea46fc2c01cadd41cd0462d137d6170
Sybre Waaijer <sybrew@users.noreply.github.com>
2019-10-19 11:50:56 +0200

    Updated trends to process new feed type.

--- inc/classes/trends.class.php
+++ inc/classes/trends.class.php
@@ -83,7 +83,8 @@ final class Trends {
 			return -1;
 
 		$transient_name = 'tsfem_latest_seo_feed';
-		$output = \get_transient( $transient_name );
+		$output         = \get_transient( $transient_name );
+		$output = false;
 
 		//* Bypass cache on AJAX as multi-admin can interfere.
 		if ( false === $ajax && false !== $output )
@@ -112,7 +113,7 @@ final class Trends {
 		$options = LIBXML_NOCDATA | LIBXML_NOBLANKS | LIBXML_NOWARNING | LIBXML_NONET | LIBXML_NSCLEAN;
 		$xml = simplexml_load_string( $xml, 'SimpleXMLElement', $options );
 
-		if ( ! isset( $xml->entry ) || empty( $xml->entry ) ) {
+		if ( empty( $xml->entry ) ) {
 			//* Retry in half an hour when server is down.
 			\set_transient( $transient_name, '', HOUR_IN_SECONDS / 2 );
 			return '';
@@ -121,11 +122,11 @@ final class Trends {
 		$entry = $xml->entry;
 		unset( $xml );
 
-		$output = '';
+		$output   = '';
 		$a_output = [];
 
 		$max = 15;
-		$i = 0;
+		$i   = 0;
 		foreach ( $entry as $object ) :
 
 			if ( $i >= $max )
@@ -161,7 +162,7 @@ final class Trends {
 				if ( 'text/html' === $type ) {
 
 					$rel = ! empty( $link_object['@attributes']['rel'] ) ? $link_object['@attributes']['rel'] : '';
-					if ( 'replies' === $rel ) {
+					if ( in_array( $rel, [ 'self', 'alternate' ], true ) ) {
 
 						$link = ! empty( $link_object['@attributes']['href'] ) ? $link_object['@attributes']['href'] : '';
 						if ( $link )
@@ -182,13 +183,14 @@ final class Trends {
 			$title = isset( $object->title ) ? $object->title->__toString() : '';
 			$title = $title ? \the_seo_framework()->escape_title( $title ) : '';
 
+			if ( ! $title )
+				continue;
+
 			$content = isset( $object->content ) ? $object->content->__toString() : '';
 			$content = $content ? \wp_strip_all_tags( $content ) : '';
 			unset( $object );
 
-			$length = 250;
-			//* Do not care for the current length. Always trim.
-			$content = \the_seo_framework()->trim_excerpt( $content, $length + 1, $length );
+			$content = \the_seo_framework()->trim_excerpt( $content, 0, 300 );
 			$content = \the_seo_framework()->escape_description( $content );
 
 			//* No need for translations, it's English only.
